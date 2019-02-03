56347c76264c59af3edd925ea83c3eea318fb973
ksugawara61 <katsuya@kokohadoko.net>
2019-02-03 19:43:52 +0900

    [add] sample script for vue js

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -16,6 +16,7 @@ window.Vue = require('vue');
  */
 
 Vue.component('example-component', require('./components/ExampleComponent.vue'));
+Vue.component('hello-world-component', require('./components/HelloWorldComponent.vue'));
 
 const app = new Vue({
     el: '#app'
